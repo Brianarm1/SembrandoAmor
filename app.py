902afd35357a82adc6b87b92436824cf52036066
carlos <Carlosk20@gmail.com>
2024-02-24 22:27:50 -0500

    se organizan todos los templates, se reescalan las imagenes

--- app.py
+++ app.py
@@ -4,5 +4,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
+app.static_folder = 'assets'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/productos')
+def productos():
+    return render_template('productos.html')
+
 if __name__ == '__main__':
     app.run(debug=True,port=5001);
